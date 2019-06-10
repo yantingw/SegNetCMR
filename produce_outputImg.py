10740cb2f8b150bb87e66b02abda87ecee1b6390
yantingw <tina0405tina@gmail.com>
2019-06-10 17:39:21 +0800

    commit all

--- produce_outputImg.py
+++ produce_outputImg.py
@@ -83,7 +83,7 @@ with tf.Session() as sess:
         result_soft = sess.run( [softmax_logits] , feed_dict=feed_dict)
         
         
-        result_soft = np.array(result_soft)
+        result_soft = np.array(result_soft).reshape(6,256,256,2)
         predict_img =np.full((6,256,256), 0)
 
 
